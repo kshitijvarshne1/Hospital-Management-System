0482ba0a00400a87433f0bac36a2247701ae4e2d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-27 17:04:35 +0530

    create DbOperations.java

---
+++ src/main/java/com/example/hospitalmanagementsystem/Repository/DbOperations.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 27-Apr-21
+ *   Time: 5:04 PM
+ *   File: DbOperations.java
+ */
+
+package com.example.hospitalmanagementsystem.Repository;
+
+public class DbOperations {
+}
+
