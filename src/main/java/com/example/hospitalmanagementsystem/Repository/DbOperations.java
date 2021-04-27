45b2eaaf4db3c4679f3fef725138ac41ef7a4df8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-27 17:19:15 +0530

    add getConnection and closeConnection Method

--- src/main/java/com/example/hospitalmanagementsystem/Repository/DbOperations.java
+++ src/main/java/com/example/hospitalmanagementsystem/Repository/DbOperations.java
@@ -7,6 +7,31 @@
 
 package com.example.hospitalmanagementsystem.Repository;
 
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Repository
 public class DbOperations {
+    private static final String URL = "jdbc:mysql://localhost:3306/patientsDB";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1122";
+    private static Connection connection;
+
+    public Connection getConnection() throws SQLException {
+        if (connection == null) {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }
+        return connection;
+    }
+
+    public void closeConnection() {
+        if (connection != null) {
+            connection = null;
+        }
+    }
+
 }
 
