c24f375f80d2228a9a49eac7254768a06285d072
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-27 16:48:29 +0530

    create PatientController.java

---
+++ src/main/java/com/example/hospitalmanagementsystem/Controller/PatientController.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 27-Apr-21
+ *   Time: 4:48 PM
+ *   File: PatientController.java
+ */
+
+package com.example.hospitalmanagementsystem.Controller;
+
+public class PatientController {
+}
+
