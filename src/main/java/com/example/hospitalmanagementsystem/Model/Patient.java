897f4e7d53ece87ca65f20febccbeaf8861e21da
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-27 17:08:07 +0530

    create Patient.java

---
+++ src/main/java/com/example/hospitalmanagementsystem/Model/Patient.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 27-Apr-21
+ *   Time: 5:07 PM
+ *   File: Patient.java
+ */
+
+package com.example.hospitalmanagementsystem.Model;
+
+public class Patient {
+}
+
